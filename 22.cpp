07524b7ef1e6e041c95bcee7f9fa3b865fc2f880
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-01-07 07:30:48 +1030

    added problem 22, need consider why timing out. Try printing each generated combination.

---
+++ 22.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    
+    vector<string> generateParenthesis(int n) {
+        
+        // - write each combination to buffer
+        std::string buf(n*2, '.');
+        int i = 0;
+        
+        // - store each combination
+        std::vector<std::string> combinations;
+        
+        // - store no. of 'moves' can make
+        int writes = n;
+        
+        // 'initialise' the sequence
+        buf[i] = '.'; 
+        
+        while(true)
+        {
+            // - negative index indicates all paths travelled
+            if(i<0) break;
+            
+            // end reached if no writes left
+            if(writes==0)
+            {
+                for(unsigned int idx = i; idx<buf.length(); idx++) { buf[idx] = ')'; }
+                combinations.push_back(buf);
+                for(unsigned int idx = i; idx<buf.length(); idx++) { buf[idx] = '.'; }
+                
+                buf[i] = '.';
+                writes--;
+                i--;
+            }
+            
+            else if(buf[i]=='.')
+            {
+                buf[i] = '(';
+                writes--;
+                i++;
+            }
+        
+            else if(buf[i]=='(')
+            {
+                buf[i]==')';
+                i++;
+            }
+            
+            else if(buf[i]==')')
+            {
+                buf[i]=='.';
+                writes--;
+                i--;
+            }            
+        }
+        
+        return combinations;
+        
+    }
+};
