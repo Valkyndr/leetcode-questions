117b42ef176b60cc39b8df0f9506ab2d74a82a37
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-02-03 19:45:30 +1030

    finished loop to get nodePtr pointing to minimum value, next step is to add on a new ListNode* in merged_list based on this value

--- 23.cpp
+++ 23.cpp
@@ -8,9 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include<algorithm>
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
+#define MAX_VALUE string::npos
 
 struct ListNode 
 {
@@ -39,12 +42,32 @@ class NodePtr
         if(node!=nullptr) node = node -> next;
     }
     
+    // return true only if held ListNode* has a non-nullptr next state
+    bool canAdvance() const
+    {
+        if(node==nullptr) return false;
+        return (node->next!=nullptr);
+    }
+
     // return value held by the handled ListNode
     int value() const
     {
+        if(node==nullptr) return MAX_VALUE;
         return node -> val;
     }
 };
+
+// helper to see if all NodePtrs in some collection are 'finished'
+bool stillRunning(const vector<NodePtr*> node_ptrs)
+{
+    for(int idx=0; idx<node_ptrs.size(); idx++)
+    {
+        if(node_ptrs[idx]->canAdvance()) return true;
+    }
+
+    // only false if all non-advancing
+    return false;
+}
 
 class Solution {
 public:
@@ -60,6 +83,30 @@ public:
         for(int idx=0; idx<lists.size(); idx)
         {
             ptrs[idx] = new NodePtr(lists[idx]);
+        }
+
+        // declare loop buffers here
+        NodePtr* min_ptr;
+        int min_value;
+
+        // in each loop, exactly 1 node_ptr must advance
+        while(stillRunning(ptrs))
+        {
+            // min_value should always be replaced
+            min_value = MAX_VALUE;
+
+            for(int idx = 0; idx<ptrs.size(); idx++)            
+            {
+                if(ptrs[idx]->canAdvance())
+                {
+                    if(ptrs[idx]->value()<min_value)
+                    {
+                        min_ptr = ptrs[idx];
+                    }
+                }
+            }
+
+            // create new node here ('add on' to merged_list)
         }
     }
 };
