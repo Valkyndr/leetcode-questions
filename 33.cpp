a9ae6c4baee154820dad5e836f64e63db9b98417
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-05-19 14:04:50 +0930

    added std::move calls where appropriate (such as popping off from a stack), and added a comment to make checks

--- 33.cpp
+++ 33.cpp
@@ -13,15 +13,22 @@ public:
     int search(std::vector<int>& nums, int target)
     {
         std::stack<range> ranges;
-        range* buffer;
+        range const *buf;
 
         // initiate sequence by placing first range (indices to first and last elements on stack)
-        ranges.push(std::make_tuple<int,int>(0, ranges.size()-1));
+        ranges.push(std::move(std::make_tuple<int,int>(0, ranges.size()-1)));
 
         // as long as have ranges not explored...
         while(!ranges.empty())
         {
-            buffer = ranges.pop(); 
+            // retrive top range
+            buf = std::move(&ranges.top()); ranges.pop();
+
+            // make checks here
+
+            /*
+                1.  if buf[0] == buf[1] then terminate this stack (skip to next loop)
+            */
 
             // get middle index
             int split_idx = 
