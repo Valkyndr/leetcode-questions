6c432c7ccfcd52c719e893aa4ce864da919d0986
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-05-25 08:57:46 +0930

    static_assert failing at line 19, ensure parameters are a constant expression at compile time, try Googling if parameters can be into static_assert

--- 33.cpp
+++ 33.cpp
@@ -12,11 +12,32 @@ class Solution {
     class Subrange
     {
         // alias to some type suited for indices
-        typedef size_t idx_t;        
+        typedef size_t idx_t; 
+
+        static constexpr idx_t get_mid(const idx_t _start, const idx_t _end)
+        {
+            static_assert(_start<=_end);
+            const idx_t length = _end - _start;
+            return (length-length%2)/2;
+        }
+
+        // expose const indices
+        const idx_t start, mid, end;
+
+        Subrange& make_subrange(idx_t _start, idx_t _end)
+        {
+            idx_t t = start;
+        }
 
         public:
 
-        Subrange
+        Subrange() = delete;
+        template<typename T> Subrange()
+        {}
+        
+        // 
+        Subrange& make_left_range();
+        Subrange& make_right_range();
 
     };
 
