aadcba81583e2b1b26e161f377f780acbb301441
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-05-24 22:45:40 +0930

    began writing code for Subrange class, but considering a rewrite with emphasis on creating Subranges from other Subranges first --- Care should be taken that Subrange is itself NOT a template class due to separate instantiations of such a class via different template parameters being treated as different, incompatible classes

--- 33.cpp
+++ 33.cpp
@@ -8,7 +8,32 @@ class Solution {
     // alias to an explicit std::range instantiation
     typedef std::tuple<int, int> range;
 
-    // light-weight class to access a subrange of some vector
+    // light-weight class to access a subrange of some indexable object
+    class Subrange
+    {
+        // alias to some type suited for indices
+        typedef size_t idx_t;        
+
+        // 
+        const idx_t& start_idx;
+        const idx_t& mid_idx;
+        const idx_t& end_idx; 
+
+        public:
+        
+        template<typename CONTAINER_TYPE> Subrange(CONTAINER_TYPE&& container)
+        {
+            
+        }
+
+        // return a Subrange object from some 
+        template<typename CONTAINER_TYPE> Subrange make_subrange_of(CONTAINER_TYPE&& container)
+        {
+            
+
+            return std::move(...);
+        }
+    };
 
 public:
     
