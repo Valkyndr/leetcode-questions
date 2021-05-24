14c402476a1fb0f64aef0ddf281f55aaceb4e929
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-05-24 22:09:11 +0930

    removed code in search() and inserted comment for creation of some lightweight class used to access a subrange of some pre-existing entity

--- 33.cpp
+++ 33.cpp
@@ -8,38 +8,12 @@ class Solution {
     // alias to an explicit std::range instantiation
     typedef std::tuple<int, int> range;
 
+    // light-weight class to access a subrange of some vector
+
 public:
     
     int search(std::vector<int>& nums, int target)
     {
-        std::stack<range> ranges;
-        range const *buf;
-
-        // initiate sequence by placing first range (indices to first and last elements on stack)
-        ranges.push(std::move(std::make_tuple<int,int>(0, ranges.size()-1)));
-
-        // as long as have ranges not explored...
-        while(!ranges.empty())
-        {
-            // retrive top range
-            buf = std::move(&ranges.top()); ranges.pop();
-
-            // make checks here
-
-            /*
-                1.  if buf[0] == buf[1] then terminate this stack (skip to next loop)
-            */
-
-            // calculate length between indices
-            const int n = std::get<int>(*(buf));
-
-            // remember: RIGHT sub-divided range includes the index
-
-            // get middle index
-            int split_idx = (n-n%2)/2;
-
-        }
-
        
     }
 };
