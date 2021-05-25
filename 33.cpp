4a168b915225ae75aafacaccf1878aebbd990377
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-05-26 02:53:11 +0930

    for simplicity, removed template definition

--- 33.cpp
+++ 33.cpp
@@ -6,15 +6,16 @@
 
 class Solution {
 
-    // alias to an explicit std::range instantiation
-    typedef std::tuple<int, int> range;
-
     // light-weight class to access a subrange of some indexable object
     class Subrange
     {
         // alias to some type suited for indices
         typedef size_t idx_t; 
 
+        // hold some reference to some container here
+        const int& reference;
+
+        // calculate index to midpoint (this value is same whether even/odd length)
         static constexpr idx_t get_mid(const idx_t _start, const idx_t _end)
         {
             // params are non-negative and ordered
@@ -29,29 +30,12 @@ class Solution {
         // expose const indices
         const idx_t start, mid, end;
 
-        Subrange& make_subrange(idx_t _start, idx_t _end)
-        {
-            idx_t t = start;
-        }
-
         public:
-
-        Subrange() = delete;
-        template<typename T> Subrange()
-        {}
-        
-        // 
-        Subrange& make_left_range();
-        Subrange& make_right_range();
 
     };
 
-public:
-    
-    int search(std::vector<int>& nums, int target)
-    {
-       
-    }
+    public:
+
 };
 
 int main()
