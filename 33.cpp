3ad5e8a468423c3d8a4a45afba83075c572d9963
Aiman Asyraaf <a1732609@student.adelaide.edu.au>
2021-06-01 01:46:45 +0930

    defined basic interface for SUbrange class (i.e. constructor)

--- 33.cpp
+++ 33.cpp
@@ -10,6 +10,28 @@ class Solution {
     // light-weight class to access a subrange of some indexable object
     class Subrange
     {
+        // typedefs / aliases
+        typedef size_t idx_t;
+
+        idx_t get_mid_of(const idx_t lbound, const idx_t rbound) const
+        {
+            return 
+        }
+
+        const idx_t left, mid, right;
+        const size_t length; 
+
+        public:
+
+        Subrange() = delete;
+        Subrange(const idx_t _left, const idx_t _right):
+            left(_left), right(right),
+            mid(get_mid_of(_left, _right)), 
+            length(_right - _left + 1)
+            {
+                assert(_left>=0);
+                assert(_right>=_left);
+            }
 
     };
 
